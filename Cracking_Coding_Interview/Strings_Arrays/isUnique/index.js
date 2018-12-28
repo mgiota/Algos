49fb2712145cb3b8319121be9867aab73be6d165
giota-cliqz <panagiota@cliqz.com>
2018-12-28 11:04:28 +0200

    1.1 Unique characters

--- Cracking_Coding_Interview/Strings_Arrays/isUnique/index.js
+++ Cracking_Coding_Interview/Strings_Arrays/isUnique/index.js
@@ -49,11 +49,52 @@ function isUniqueBooleanFlags(str) {
 }
 
 function isUniqueBitwise(str) {
+  let checker = 0;
+  for (let ch of str) {
+    const code = ch.charCodeAt();
+    if ((checker & (1 << code)) > 0) {
+      return false;
+    }
+    checker |= (1 << code);
+  }
 
+  return true;
+}
+
+// 0 0 0 0 0 0
+// 0 1 0 0 0 0
+
+function isUniqueLast(str) {
+  //g i t a g
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] !== str[i + 1]) {
+      return false;
+    }
+  }
+
+  if (str[0] !== str[str.length - 1]) {
+    return false;
+  }
+
+  return true;
+}
+
+function isUniqueLast(str) {
+  let map = {};
+  for (let ch of str) {
+    if (ch in map) {
+      return false;
+    }
+    map[ch] = 1;
+  }
+
+  return true;
 }
 
 module.exports = {
   isUnique,
   isUniqueNoDataStrucure,
-  isUniqueBooleanFlags
+  isUniqueBooleanFlags,
+  isUniqueBitwise,
+  isUniqueLast
 }
--- Cracking_Coding_Interview/Strings_Arrays/isUnique/test.js
+++ Cracking_Coding_Interview/Strings_Arrays/isUnique/test.js
@@ -49,4 +49,38 @@ describe('#isUniqueBooleanFlags', () => {
     const result = index.isUniqueBooleanFlags('');
     expect(result).toEqual(true);
   });
-})
+});
+
+describe('#isUniqueLast', () => {
+  test('returns true if it is unique', () => {
+    const result = index.isUniqueLast('giota');
+    expect(result).toEqual(true);
+  });
+
+  test('returns false if it is not unique', () => {
+    const result = index.isUniqueLast('edessa');
+    expect(result).toEqual(false);
+  });
+
+  test('returns true if string is empty', () => {
+    const result = index.isUniqueLast('');
+    expect(result).toEqual(true);
+  });
+});
+
+describe('#isUniqueBitwise', () => {
+  test('returns true if it is unique', () => {
+    const result = index.isUniqueBitwise('giota');
+    expect(result).toEqual(true);
+  });
+
+  test('returns false if it is not unique', () => {
+    const result = index.isUniqueBitwise('edessa');
+    expect(result).toEqual(false);
+  });
+
+  test('returns true if string is empty', () => {
+    const result = index.isUniqueBitwise('');
+    expect(result).toEqual(true);
+  });
+});
