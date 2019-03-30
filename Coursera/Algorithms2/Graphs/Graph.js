94bc1683afa47fa60244132372683d6340e9e86f
giota-cliqz <panagiota@cliqz.com>
2019-03-30 23:15:15 +0100

    Coursera: garph implementation using adjecency list

--- Coursera/Algorithms2/Graphs/Graph.js
+++ Coursera/Algorithms2/Graphs/Graph.js
@@ -1,6 +1,38 @@
 class Graph {
-  constructor() {
-    this.ids = [1, 2, 3, 4, 5];
+  constructor(n) {
+    this.n = n;
+    this.adjList = Array.from(Array(n), () => []); // [ [], [], [], []] -> n = 4
+    // this.adj = Array(n);
+    // for (let i = 0; i < n; i++) {
+    //   this.adj[i] = [];
+    // }
+  }
+
+  addEdge(v, w) {
+    this.adjList[v].push(w);
+    this.adjList[w].push(v);
+  }
+
+  adj(v) {
+      return this.adj[v];
+  }
+
+  size() {
+    return this.n;
+  }
+
+  degree(v) {
+    return this.adjList[v].length;
+  }
+
+  maxDegree() {
+    let max = 0;
+    for (let i = 0; i < this.adjList.length; i++) {
+      if (this.degree(i) > max) {
+        max = this.degree(i);
+      }
+    }
+    return max;
   }
 }
 
--- Coursera/Algorithms2/Graphs/test.js
+++ Coursera/Algorithms2/Graphs/test.js
@@ -1,8 +1,26 @@
 import Graph from './Graph';
 
 describe('#Graph', () => {
-  test('Graph API', () => {
-    let graph = new Graph();
-    expect(graph.ids.length).toEqual(5);
+  test('constructor length', () => {
+    let graph = new Graph(5);
+    expect(graph.size()).toEqual(5);
+  });
+
+  test('Graph maxDegree', () => {
+    let graph = new Graph(3);
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 2);
+
+    expect(graph.maxDegree()).toEqual(2);
+  });
+
+  test('Graph degree', () => {
+    let graph = new Graph(3);
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 2);
+
+    expect(graph.degree(2)).toEqual(1);
+    expect(graph.degree(1)).toEqual(1);
+    expect(graph.degree(0)).toEqual(2);
   });
 });
