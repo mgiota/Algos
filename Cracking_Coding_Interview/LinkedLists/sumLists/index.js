ed7dc5331dc0213be9b63a5e9b4c0414b75d56ce
giota-cliqz <panagiota@cliqz.com>
2020-05-05 21:46:51 +0200

    Boook: LinkedLists: sumLists iterative

--- Cracking_Coding_Interview/LinkedLists/sumLists/index.js
+++ Cracking_Coding_Interview/LinkedLists/sumLists/index.js
@@ -25,6 +25,52 @@ const sumLists = (l1, l2, carry = 0) => {
   return result;
 }
 
+const sumListsIterative = (n1, n2) => {
+  let carry = 0;
+  let sum;
+  let result = null;
+  let prev = null;
+  let temp = null;
+
+  while (n1 !== null || n2 !== null) {
+    sum = carry;
+    if (n1 !== null) {
+      sum += n1.data;
+    }
+
+    if (n2 !== null) {
+      sum += n2.data;
+    }
+
+    carry = (sum >= 10) ? 1 : 0;
+    sum = sum % 10;
+    temp = new LinkedListNode(sum);
+
+    if (result === null) {
+      result = temp;
+    } else {
+      prev.next = temp;
+    }
+
+    prev = temp;
+
+    if (n1 !== null) {
+      n1 = n1.next;
+    }
+
+    if (n2 !== null) {
+      n2 = n2.next;
+    }
+  }
+
+  if (carry > 0) {
+    temp.next = new LinkedListNode(carry);
+  }
+  console.log(result)
+  return result;
+}
+
 module.exports = {
-  sumLists
+  sumLists,
+  sumListsIterative
 }
--- Cracking_Coding_Interview/LinkedLists/sumLists/test.js
+++ Cracking_Coding_Interview/LinkedLists/sumLists/test.js
@@ -3,24 +3,64 @@ import LinkedList from '../../../Data_structures/LinkedList/LinkedList';
 import { LinkedListNode } from '../../../Data_structures/LinkedList/LinkedList';
 
 describe('#sum lists recursive', () => {
-    test('#', () => {
-      const n1_1 = new LinkedListNode(7);
-      const n1_2 = new LinkedListNode(1);
-      const n1_3 = new LinkedListNode(6);
+  test('#', () => {
+    const n1_1 = new LinkedListNode(7);
+    const n1_2 = new LinkedListNode(1);
+    const n1_3 = new LinkedListNode(6);
 
-      n1_1.next = n1_2;
-      n1_2.next = n1_3;
+    n1_1.next = n1_2;
+    n1_2.next = n1_3;
 
-      const n2_1 = new LinkedListNode(5);
-      const n2_2 = new LinkedListNode(9);
-      const n2_3 = new LinkedListNode(2);
+    const n2_1 = new LinkedListNode(5);
+    const n2_2 = new LinkedListNode(9);
+    const n2_3 = new LinkedListNode(2);
 
-      n2_1.next = n2_2;
-      n2_2.next = n2_3;
+    n2_1.next = n2_2;
+    n2_2.next = n2_3;
 
-      const res = index.sumLists(n1_1, n2_1);
-      expect(res.data).toEqual(2);
-      expect(res.next.data).toEqual(1);
-      expect(res.next.next.data).toEqual(9);
-    });
+    const res = index.sumLists(n1_1, n2_1);
+    expect(res.data).toEqual(2);
+    expect(res.next.data).toEqual(1);
+    expect(res.next.next.data).toEqual(9);
+  });
+});
+
+describe('#sum lists iterative', () => {
+  test('#testcase 1', () => {
+    const n1_1 = new LinkedListNode(7);
+    const n1_2 = new LinkedListNode(1);
+    const n1_3 = new LinkedListNode(6);
+
+    n1_1.next = n1_2;
+    n1_2.next = n1_3;
+
+    const n2_1 = new LinkedListNode(5);
+    const n2_2 = new LinkedListNode(9);
+    const n2_3 = new LinkedListNode(2);
+
+    n2_1.next = n2_2;
+    n2_2.next = n2_3;
+
+    const res = index.sumListsIterative(n1_1, n2_1);
+    expect(res.data).toEqual(2);
+    expect(res.next.data).toEqual(1);
+    expect(res.next.next.data).toEqual(9);
+  });
+
+  test('#testcase 2', () => {
+    const n1_1 = new LinkedListNode(3);
+    const n1_2 = new LinkedListNode(1);
+
+    n1_1.next = n1_2;
+
+    const n2_1 = new LinkedListNode(8);
+    const n2_2 = new LinkedListNode(9);
+
+    n2_1.next = n2_2;
+
+    const res = index.sumListsIterative(n1_1, n2_1);
+    expect(res.data).toEqual(1);
+    expect(res.next.data).toEqual(1);
+    expect(res.next.next.data).toEqual(1);
+  });
 });
